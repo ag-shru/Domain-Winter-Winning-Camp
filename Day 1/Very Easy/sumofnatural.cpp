2941de56e32d7dfe570082dcb5b8f0875d35c010
Shruti Agarwal <shrutiagarwal5956@gmail.com>
2024-12-24 11:16:53 +0530

    Add files via upload

---
+++ Day 1/Very Easy/sumofnatural.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter value of n: ";
+    cin>>n;
+    cout<<"Sum of first n natural numbers: ";
+    cout<<n*(n+1)/2;
+    return 0;
+}
